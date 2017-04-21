7da6acac2911712561095a8a951924b6bb60a922
madisonlutz <mrl440@nyu.edu>
2017-04-21 14:10:56 -0400

    Mini Game 1
    Starting my first mini game. This game is a search and find where the
    user tries to find a frog in the leaves. I currently am having issues
    with the camera tracking.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,63 @@
-function setup() { 
-  createCanvas(400, 400);
-} 
+//virtual camera
+//move the mouse around
 
-function draw() { 
-  background(0);
+
+var bg;
+var frame;
+var leaves;
+//the scene is twice the size of the canvas
+var SCENE_W = 1600;
+var SCENE_H = 800;
+
+
+
+
+function preload(){
+ 	frame = loadImage("assets/frame.png");
+ 	leaves = loadImage("assets/leaves.png")
+
+}
+
+function setup() {
+  createCanvas(800,400);
+   
+
+  
+ 
+}
+
+function draw() {
+  background(0,81,21); 
+  
+
+  
+
+  
+  //a camera is created automatically at the beginning
+  
+  //.5 zoom is zooming out (50% of the normal size)
+  if(mouseIsPressed)
+    camera.zoom = .5;
+  else
+    camera.zoom = 1;
+  
+  //set the camera position to the mouse position
+  camera.position.x = mouseX;
+  camera.position.y = mouseY;
+  
+
+  
+ //right now I am having the issue of the camera not staying within the confines of the leaves in the background
+    
+image(leaves, 0, 0);
+  //I can turn on and off the camera at any point to restore
+  //the normal drawing coordinates, the frame will be drawn at 
+  //the absolute 0,0 (try to see what happens if you don't turn it off
+  
+    camera.off();
+
+image(frame, 0, 0);
+    
+ 
+
 }
