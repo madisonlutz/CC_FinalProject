1c88582b3d34a3a55404aa46ef7e965e98a2ad13
madisonlutz <mrl440@nyu.edu>
2017-04-22 21:49:54 -0400

    Bug Fixes & Testing P5
    I fixed some bugs and am working in game 2 to practice using the
    p5.play library. I also added some additional graphics.

--- game1.js
+++ game1.js
@@ -7,29 +7,35 @@
 var bg;
 var frame;
 var leaves;
+var frog;
+var frogimg;
 
 
 function preload(){
  	frame = loadImage("assets/frame.png");
- 	leaves = loadImage("assets/leavesbig.png")
+ 	leaves = loadImage("assets/leavesbig.png");
+    frogimg = loadImage("assets/frog01.png");
 }
 
 function setup() {
   createCanvas(1200,675);
  
+
+    frog = new Frog(); //create a new frog
+
+
+/* P5.play failed to work for me, I have not figured out why yet.
+  frog = createSprite(500, 500);
+  frog.addAnimation("normal", "assets/frog01.png",  "assets/frog02.png");
+*/
+    
 }
 
 function draw() {
   background(0,81,21); 
   
   
-  //a camera is created automatically at the beginning
-  
-  //.5 zoom is zooming out (50% of the normal size)
-  //if(mouseIsPressed)
-  //  camera.zoom = .5;
-  //else
-    camera.zoom = 1;
+  camera.zoom = 1;
   
   //set the camera position to the mouse position
   camera.position.x = mouseX + 600; //by adding half of the width to the mouseX it keeps the camera focused on the leaves without going out of frame
@@ -37,18 +43,51 @@ function draw() {
   
 
   
- //Right now I am having the issue of the camera not staying within the confines of the leaves in the background - FIXED
+ //Right now I am having the issue of the camera not staying within the confines of the leaves in the background - FIXED 04/22/17
  //I fixed this issue by add half of the height and width of the canvas to the mouse position
     
 image(leaves, 0, 0);
-  //I can turn on and off the camera at any point to restore
-  //the normal drawing coordinates, the frame will be drawn at 
-  //the absolute 0,0 (try to see what happens if you don't turn it off
-  
-    camera.off();
 
+//Play Frog -- I tried using the play library to have the frog as a sprite but it wouldn't load
+    
+//drawSprites();
+    
+    
+/*Vector frog -- I want to have it so that when you hover over the frog something happen. I was testing it out but I couldn't get it
+to work yet */
+
+
+/*
+    if ((mouseX > frog.position.x || mouseX < frog.position.x + 100) && (mouseY > frog.position.y || mouseY < frog.position.y + 100)) {
+        frog.display(0); //display the frog
+    }
+
+        else {
+            frog.display(255);
+        }
+    
+*/
+    
+frog.display();
+    
+camera.off(); //turn camera off so that the binoculars frame remains stationary
 image(frame, 0, 0, 1200, 675);
+
+
     
  
 
+}
+
+
+function Frog (){
+    
+    this.position = new createVector (random(365, 1230), random(253, 542)); //create vector to store the position of the frog
+    
+    this.display = function (){
+     image(frogimg, this.position.x, this.position.y, 100, 100); //show the frog drawing somewhere on within the leaves
+    }
+    
+    
+        
 }
--- game2.js
+++ game2.js
@@ -1,16 +1,33 @@
 // GAME 2 -- QUIZ AT THE END 
 
-function preload(){
-    
+
+//Creating animations
+
+//animations like p5 images should be stored in variables
+//in order to be displayed during the draw cycle
+var frog;
+
+//it's advisable (but not necessary) to load the images in the preload function
+//of your sketch otherwise they may appear with a little delay
+function preload() {
+
+//create an animation from a sequence of numbered images
+//pass the first and the last file name and it will try to find the ones in between
+frog = loadAnimation("assets/frog01.png", "assets/frog12.png");
+  
+}
+
+function setup() {
+  createCanvas(800,300);
     
 }
 
-function setup (){
+function draw() {
+  background(255,255,255);  
+  
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
     
-    
-}
-
-function draw(){
-    
-    
+  animation(frog, 300, 150);
+  
 }
